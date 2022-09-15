145aac01b3d8e573bbe882975c352d8c284e3e30
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-09-15 22:28:40 +0300

    Update Employee.java

--- src/main/java/com/example/VacationManagement/model/Employee.java
+++ src/main/java/com/example/VacationManagement/model/Employee.java
@@ -25,7 +25,6 @@ import java.time.LocalDate;
 public class Employee implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(nullable = false)
     private Long idEmployee;
 
     @Column(nullable = false, length = 50)
