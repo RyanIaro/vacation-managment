1b230086036090f76b613551c80489bbc0c0981d
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-09-15 18:36:45 +0300

    Create EmployeeController.java

---
+++ src/main/java/com/example/VacationManagement/controller/EmployeeController.java
@@ -0,0 +1,45 @@
+package com.example.VacationManagement.controller;
+
+import com.example.VacationManagement.model.Employee;
+import com.example.VacationManagement.service.EmployeeService;
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+@AllArgsConstructor
+@RestController
+@CrossOrigin
+public class EmployeeController {
+
+    private final EmployeeService service;
+
+    @GetMapping("/")
+    public String welcome() {
+        return "Hello wolrd!";
+    }
+
+    @GetMapping("/employees")
+    public List<Employee> getEmployees(
+            @RequestParam(value = "first_name", required = false, defaultValue = "") String firstName,
+            @RequestParam(value = "last_name", required = false, defaultValue = "") String lastName) {
+        return service.getAll(firstName, lastName);
+    }
+
+    @PutMapping("/employees")
+    public List<Employee> createOrUpdateEmployees(@RequestBody List<Employee> employees) {
+        return service.saveAll(employees);
+    }
+
+    @GetMapping("/employees/{id}")
+    public Optional<Employee> getEmployeeById(@PathVariable Long id) throws NoSuchElementException{
+        return service.getById(id);
+    }
+
+    @DeleteMapping("/employees/{id}")
+    public void deleteEmployeeById(@PathVariable Long id) throws NoSuchElementException {
+        service.deleteById(id);
+    }
+}
