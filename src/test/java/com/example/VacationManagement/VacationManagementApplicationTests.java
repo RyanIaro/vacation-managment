8f5757779129e1934a992ba2b8fc8ce0b30d3d8d
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-09-15 18:47:50 +0300

    Create VacationManagementApplicationTests.java

---
+++ src/test/java/com/example/VacationManagement/VacationManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VacationManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
