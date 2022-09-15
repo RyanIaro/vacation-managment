46b2d21ae408343bfc06c4280f05cf6f48ce66e8
Ryan Iaro <98638811+RyanIaro@users.noreply.github.com>
2022-09-15 18:38:42 +0300

    Create Employee.java

---
+++ src/main/java/com/example/VacationManagement/model/Employee.java
@@ -0,0 +1,51 @@
+package com.example.VacationManagement.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.io.Serializable;
+import java.time.LocalDate;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "\"Employees\"")
+@Getter
+@Setter
+@ToString
+public class Employee implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(nullable = false)
+    private Long idEmployee;
+
+    @Column(nullable = false, length = 50)
+    private String firstName;
+
+    @Column(nullable = false, length = 50)
+    private String lastName;
+
+    @Column(nullable = false, length = 30)
+    private String office;
+
+    @Column(nullable = false, length = 30)
+    private String department;
+
+    @Column(nullable = false)
+    private float salary;
+
+    @Column(nullable = false)
+    private LocalDate startingDate;
+
+    @Column(nullable = false)
+    private int authorizedOffDays;
+}
